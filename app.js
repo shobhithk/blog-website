a093eaa9d5aea11a6bda07cff93e1ee45670545d
shobhithk <shobhithkamata01@gmail.com>
2020-11-29 00:43:46 +0530

    2nd commit

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts=[];
 
+
+const blogSchema = new mongoose.Schema({
+  Title: String,
+  content: String
+});
+
+const Blog = mongoose.model("post", blogSchema);
 
 const app = express();
 
@@ -20,7 +29,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
-  res.render("home", {homeText:homeStartingContent, posts:posts});
+  
+  Blog.find({},(err,data)=>{
+    if(!err){
+      res.render("home", {homeText:homeStartingContent, posts:data});
+    }
+  });
+
+ 
   
 });
 
@@ -37,22 +53,26 @@ app.get("/compose", (req,res)=>{
 });
 
 app.post("/compose", (req,res)=>{
-  const post={
+  const post= new Blog({
     Title: req.body.postTitle,
     content: req.body.postText
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  console.log(post);
+   post.save((err)=>{
+    if(!err) res.redirect("/");
+   });
+   
 });
 
-app.get("/posts/:days",(req,res)=>{
-  let parameter=req.params.days;
-  parameter = _.lowerCase(parameter);
+app.get("/posts/:_id",(req,res)=>{
+  let parameter=req.params._id;
+  
+Blog.findOne({_id: parameter},(err,data)=>{
+    if(!err){
+      res.render("post", {post:data});
+    }
+})
 
-posts.forEach((post)=>{
-  if(_.lowerCase(post.Title) === parameter)
-      res.render("post", {post:post});
-});
   
 });
 
